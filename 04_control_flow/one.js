1b3c99b2a676398eff4974d0a91430c5b6ef9ab4
KONAIN-WORSI <konainworsi@gmail.com>
2025-12-24 21:31:41 +0545

    control flow added

---
+++ 04_control_flow/one.js
@@ -0,0 +1,28 @@
+// if
+
+// if(condition) {
+
+// }else if(condition){
+
+// }
+
+// <, >, =, ==, !=, === Operators
+const any = 89
+
+// switch(any){
+//     case 1:
+//         console.log("many");
+        
+//     case 2:
+//         console.log("too many");
+        
+//     case 3: 
+//         console.log("very too many");
+        
+//     default:
+//         console.log("nothing");
+        
+// }
+
+
+
