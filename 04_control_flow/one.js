aa2a5751368a160278d283b84c92698d6c2e00da
KONAIN-WORSI <konainworsi@gmail.com>
2025-12-25 22:34:51 +0545

    for loop starting

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -7,6 +7,14 @@
 // }
 
 // <, >, =, ==, !=, === Operators
+
+// falsy values :
+// false, 0 , "", null, undefined, NaN, -0, BigInt 0n
+
+// truthy value:
+// true, 1, " ", [], {}, function() {}, 'false', "0"
+
+
 const any = 89
 
 // switch(any){
@@ -23,6 +31,26 @@ const any = 89
 //         console.log("nothing");
         
 // }
+
+
+// Nullish coalescing operator (??): null or un defined
+
+let val1;
+
+
+
+// val1 = null ?? 26;
+val1 = undefined ?? 78;
+
+// console.log(val1);
+
+//Ternary Operator
+
+let age = 48;
+
+let ageChecker = age >= 18 ? "You can vote" : "you cannot vote";
+
+// console.log(ageChecker);
 
 
 
---
+++ 05_iterations/one.js
@@ -0,0 +1,22 @@
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+    
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Outer loop value ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`${i} * ${j} = ${i * j}`);
+        
+//     }
+    
+// }
+
+const arr = ["saktiman", "naagraj", "doga", "shaktimaan", "chacha bhatija"];
+
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    console.log(element);
+    
+}
